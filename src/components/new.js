419d24ccfbc1a1d28d48fd75a306a60abce0773f
Code With Neeraj <jangraneeraj83@gmail.com>
2024-07-17 01:38:08 +0530

    whole code

---
+++ src/components/new.js
@@ -0,0 +1,16 @@
+
+
+// console.log(a);
+// var a=6;
+// console.log(v);
+// let v = 7;
+
+let a1 = 10;
+let a2 = 20;
+function s(a3,b){
+    console.log(a3+b)
+}
+
+function a(a1,a2){
+    s(a1,a2)
+};
